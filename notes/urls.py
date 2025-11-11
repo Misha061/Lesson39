c0f82a540f09ed2c5aaf259411547ba2ab553b36
Misha061 <113469810+Misha061@users.noreply.github.com>
2025-11-11 21:25:03 +0200

    Lesson 39 Homework

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import note_view
+from .views import note_view, remarks
 
 urlpatterns = [
     path('notes/', note_view, name='notes'),
+    path("remarks/", remarks),
 
 ]
--- notes/views.py
+++ notes/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def note_view(request):
     return HttpResponse("Hello from Notes app")
+
+def remarks(request): ### Lesson39
+
+    return render(request, "lesson39.html", {"name":"Misha", "notes" : [{"text" : "Make homework"}, {"text":"Go to the shop"}]})
+
